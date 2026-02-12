919776a36bd2e122893b5447b3958a3f562fd681
ShushunovAleksei <shushunov_an@phystech.edu>
2026-02-12 19:53:55 +0300

    PWD

---
+++ get-led/pwm-led.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+led = 26
+GPIO.setup(13, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
+
+botton = 13
+state = 0
+while True:
+    if GPIO.input(botton):
+        state = not state
+        GPIO.output(led, state)
+        time.sleep(0.2)
