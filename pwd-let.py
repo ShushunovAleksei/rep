1d7827940cbb0128c7f9aa9bb791987428b94f6a
ShushunovAleksei <shushunovaleksei107@gmail.com>
2026-02-19 16:20:34 +0300

    Create pwd-let.py

---
+++ pwd-let.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import
+
+led = 26
+duty = 0.0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
+
+pwm = GPIO.PWM(led, 200)
+pwm.start(duty)
+
+while True:
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.05)
+
+    duty += 1.0
+    if duty > 100.0:
+        duty = 0.0
