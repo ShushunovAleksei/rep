d070b04c978b078525e9a734f82438cd146712be
ShushunovAleksei <shushunov_an@phystech.edu>
2026-03-12 18:56:01 +0300

    доделать

--- get-dac/pwm_dac.py
+++ get-dac/pwm_dac.py
@@ -1,12 +1,15 @@
 import RPi.GPIO as GPIO
 import time
+dynamic_range = 3.290
 class PWM_DAC:
     def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
        
         self.gpio_bits = gpio_pin
         self.dynamic_range = dynamic_range
         self.verbose = verbose
-        
+        self.freq = pwm_frequency
+        self.verbose = verbose
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0) 
         
@@ -19,8 +22,9 @@ class PWM_DAC:
             print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
             print("Устанавлниваем 0.0 В")
             return 0
-    number = int(voltage / dynamic_range * 255)
-    self.set_number(number)
+        number = int(voltage / dynamic_range * 255)
+        self.set_number(number)
+    
         
 
 if __name__ == "__main__":
